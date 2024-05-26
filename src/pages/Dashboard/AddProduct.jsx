72d2a34c061a8882aaa96ddc9f92b916cc1f71fb
mehedi hasan <mdmehedihasananik111@gmail.com>
2024-05-26 09:24:42 +0600

    add product page update [DONE]

--- src/pages/Dashboard/AddProduct.jsx
+++ src/pages/Dashboard/AddProduct.jsx
@@ -1,5 +1,93 @@
 const AddProduct = () => {
-  return <div>Add Product</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const id = form.id.value;
+    const title = form.title.value;
+    const brand = form.brand.value;
+    const number = form.number.value;
+    const description = form.description.value;
+    const image_url = form.image_url.value;
+
+    const formData = { id, title, brand, number, description, image_url };
+
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-center">Add a Product</h1>
+      <div className="my-16 min-h-screen">
+        <form onSubmit={handleSubmit}>
+          <div className="lg:flex items-center justify-center gap-16">
+            <div className="mt-4">
+              <input
+                type="text"
+                name="title"
+                placeholder="Title"
+                className="bg-gray-100 p-4 w-full lg:w-[350px] border border-black rounded-lg"
+              />
+            </div>
+            <div className="mt-4">
+              <input
+                type="text"
+                name="brand"
+                placeholder="Brand"
+                className="bg-gray-100 p-4 w-full lg:w-[350px] border border-black rounded-lg"
+              />
+            </div>
+          </div>
+
+          <div className="lg:flex items-center justify-center gap-16">
+            <div className="mt-4">
+              <input
+                type="text"
+                name="number"
+                placeholder="Price"
+                className="bg-gray-100 p-4 w-full lg:w-[350px] border border-black rounded-lg"
+              />
+            </div>
+            <div className="mt-4">
+              <input
+                type="text"
+                name="description"
+                placeholder="Description"
+                className="bg-gray-100 p-4 w-full lg:w-[350px] border border-black rounded-lg"
+              />
+            </div>
+          </div>
+
+          <div className="lg:flex items-center justify-center gap-16">
+            <div className="mt-4">
+              <input
+                type="text"
+                name="image_url"
+                placeholder="IMAGE_URL"
+                className="bg-gray-100 p-4 w-full lg:w-[350px] border border-black rounded-lg"
+              />
+            </div>
+            <div className="mt-4">
+              <input
+                type="text"
+                name="id"
+                placeholder="ID"
+                className="bg-gray-100 p-4 w-full lg:w-[350px] border border-black rounded-lg"
+              />
+            </div>
+          </div>
+
+          <div className="mt-4 flex justify-center items-center">
+            <input
+              type="submit"
+              value="Add Product"
+              className="btn mt-4 w-full lg:w-[600px] bg-red-500 text-white p-4"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddProduct;
